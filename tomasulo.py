cf2b1b852b9737d20c29e86c0673b151e7bd0a39
gabrielld06 <gabriellimadias2001@gmail.com>
2021-12-08 17:08:26 -0300

    aqui acabou

--- tomasulo.py
+++ tomasulo.py
@@ -1,5 +1,5 @@
 from collections import deque
-from typing import Deque, List, TextIO
+from typing import Deque, List, TextIO, Union
 import sys
 
 ARITMETICA : List[str] = ["ADD", "ADDI", "SUB", "SUBI", "MUL", "DIV"]
@@ -27,7 +27,7 @@ class Register:
 class DataMemory:
     def __init__(self) -> None: 
         self.Qi = None
-        self.value = 0 # TODO: rever atributos depois
+        self.value = 0
 
 class FunctionalUnit:
     def __init__(self) -> None:
@@ -39,6 +39,7 @@ class Tomasulo:
         self.pc : int = 0
         self.clock : int = 0
         self.instrucoes : int = 0
+        self.lock : bool = False
         self.rsAddI : int = 0
         self.rsMulI : int = 16
         self.rsLdI : int = 32
@@ -66,7 +67,7 @@ class Tomasulo:
 
     # Faz o despacho da instrução
     def issue(self) -> None:
-        if len(self.filaDespacho) < 1:
+        if len(self.filaDespacho) < 1 or self.lock:
             return
         inst = self.filaDespacho.popleft()
         op = inst[0]
@@ -143,47 +144,42 @@ class Tomasulo:
                     self.RS[r].Vk = self.registerStat[rt].value
             self.RS[r].A = imm
             self.RS[r].busy = True
+            self.lock = True
         elif op in MEMORIA:
-            rd = int(inst[1][1:])
-            imm, rs = inst[2].replace("(", " ").replace(")", " ").split()
-            imm = int(imm)
-            rs = int(rs[1:])
             r = 32
             while self.RS[r].busy and r < 48:
                 r += 1
             if op == "LW":
-                """
-                if self.registerStat[rs].Qi != 0:
-                    self.RS[r].Qj = self.registerStat[rs].Qi
-                else:
-                    self.RS[r].Vj = self.registerStat[rs].value
-                    self.RS[r].Qj = 0
-                """
+                rd = int(inst[1][1:])
+                imm, rs = inst[2].replace("(", " ").replace(")", " ").split()
+                imm = int(imm)
+                rs = int(rs[1:])
                 self.RS[r].Vj = self.memory[rs].value
                 self.RS[r].A = imm
                 self.RS[r].busy = True
                 self.registerStat[rd].Qi = r
             elif op == "SW":
-                """
-                if self.registerStat[rs].Qi != 0:
+                print(inst)
+                rs = int(inst[1][1:])
+                imm, rd = inst[2].replace("(", " ").replace(")", " ").split()
+                imm = int(imm)
+                rd = int(rd[1:])
+                if self.registerStat[rs].Qi != None:
                     self.RS[r].Qj = self.registerStat[rs].Qi
                 else:
                     self.RS[r].Vj = self.registerStat[rs].value
-                    self.RS[r].Qj = 0
-                """
-                self.RS[r].Vj = self.registerStat[rs].value
                 self.RS[r].A = imm
                 self.RS[r].busy = True
                 self.memory[rd].Qi = r
         else:
-            print(op) # TODO : remove
             raise Exception("Instrução não reconhecida")
         self.RS[r].exec = -1
         self.RS[r].op = op
         self.instrucoes += 1
 
     # Executa as instruções
-    def setUnits(self): 
+    def setUnits(self) -> None: 
+        # Controle de tempo
         for unit in self.addUnit:
             if unit.busy:
                 self.RS[unit.inst].exec -= 1
@@ -231,7 +227,7 @@ class Tomasulo:
                     self.RS[self.rsMulI].exec = 25
             self.rsMulI = ((self.rsMulI + 1) % 16) + 16
         for i in range(16):
-            if self.RS[self.rsLdI].busy and self.RS[self.rsLdI].exec == -1:
+            if self.RS[self.rsLdI].busy and self.RS[self.rsLdI].exec == -1 and self.RS[self.rsLdI].Vj != None:
                 if not self.ldUnit[0].busy:
                     self.ldUnit[0].inst = self.rsLdI
                     self.ldUnit[0].busy = True
@@ -247,7 +243,8 @@ class Tomasulo:
                 self.RS[self.rsLdI].exec = 5
             self.rsLdI = ((self.rsLdI + 1) % 16) + 32
 
-    def execute(self, inst : int):
+    # Devolve o valor resultante da operação da instrução
+    def execute(self, inst : int) -> Union[int, bool]:
         if self.RS[inst].op == "ADD":
             return self.RS[inst].Vj + self.RS[inst].Vk
         elif self.RS[inst].op == "SUB":
@@ -271,24 +268,28 @@ class Tomasulo:
         elif self.RS[inst].op == "SW":
             return self.RS[inst].Vj + self.RS[inst].A
         elif self.RS[inst].op == "BLT":
-            return self.RS[inst].A if self.RS[inst].Vj < self.RS[inst].Vk else self.pc
+            return self.RS[inst].Vj < self.RS[inst].Vk
         elif self.RS[inst].op == "BGT":
-            return self.RS[inst].A if self.RS[inst].Vj > self.RS[inst].Vk else self.pc
+            return self.RS[inst].Vj > self.RS[inst].Vk
         elif self.RS[inst].op == "BEQ":
-            return self.RS[inst].A if self.RS[inst].Vj == self.RS[inst].Vk else self.pc
+            return self.RS[inst].Vj == self.RS[inst].Vk
         elif self.RS[inst].op == "BNE":
-            return self.RS[inst].A if self.RS[inst].Vj != self.RS[inst].Vk else self.pc
+            return self.RS[inst].Vj != self.RS[inst].Vk
         elif self.RS[inst].op == "J":
-            return self.RS[inst].A
+            return True
         else:
             pass
-
-    def write(self):
+    
+    # Faz a varredura das dependencias e escreve o resultado da instrução
+    def write(self) -> None:
         for unit in self.addUnit:
             if unit.busy and self.RS[unit.inst].exec == 0:
                 v : int = self.execute(unit.inst)
                 if self.RS[unit.inst].op in DESVIO:
-                    self.pc = v
+                    if v:
+                        self.pc = self.RS[unit.inst].A
+                        self.filaDespacho = deque()
+                    self.lock = False
                 else:
                     for register in self.registerStat:
                         if register.Qi == unit.inst:
@@ -347,7 +348,30 @@ class Tomasulo:
                 self.RS[unit.inst] = ReservationStation()
                 unit.busy = False
 
-    def printStatus(self):
+    # Escreve na tela o status das Estações de Reserva, dos Registradores e da Memoria
+    def printStatus(self) -> None:
+        print("Clock: ", self.clock)
+        print("    RS    | BUSY  | Clock |   OP   |   Vj  |   Vk  |   Qj  |   Qk  |   A   |")
+        for i in range(16):
+            print("ADD  | {:2d} | {!r:5} | {!r:5} | {!r:6s} | {!r:5} | {!r:5} | {!r:5} | {!r:5} | {!r:5} |".format(i, self.RS[i].busy, self.RS[i].exec, self.RS[i].op, self.RS[i].Vj, self.RS[i].Vk, self.RS[i].Qj, self.RS[i].Qk, self.RS[i].A))
+        for i in range(16, 32):
+            print("MUL  | {:2d} | {!r:5} | {!r:5} | {!r:6s} | {!r:5} | {!r:5} | {!r:5} | {!r:5} | {!r:5} |".format(i, self.RS[i].busy, self.RS[i].exec, self.RS[i].op, self.RS[i].Vj, self.RS[i].Vk, self.RS[i].Qj, self.RS[i].Qk, self.RS[i].A))
+        for i in range(32, 48):
+            print("LOAD | {:2d} | {!r:5} | {!r:5} | {!r:6s} | {!r:5} | {!r:5} | {!r:5} | {!r:5} | {!r:5} |".format(i, self.RS[i].busy, self.RS[i].exec, self.RS[i].op, self.RS[i].Vj, self.RS[i].Vk, self.RS[i].Qj, self.RS[i].Qk, self.RS[i].A))
+        
+        print("\nRegistradores:")
+        print("Reg | Qi | Value")
+        for i in range(len(self.registerStat)):
+            print("{:3d} | {!r:5} | {:3d} ".format(i, self.registerStat[i].Qi, self.registerStat[i].value))
+       
+        print("\nMemoria:")
+        print("Mem | Qi | Value")
+        for i in range(len(self.memory)):
+            print("{:3d} | {!r:5} | {:3d} ".format(i, self.memory[i].Qi, self.memory[i].value))
+
+    # Escreve na tela o status das Estações de Reserva ocupadas, 
+    # dos 16 Primeiros Registradores e das 16 Primeiras Celulas de Memoria
+    def printBusyStatus(self) -> None:
         print("Clock: ", self.clock)
         print("    RS    | BUSY  | Clock |   OP   |   Vj  |   Vk  |   Qj  |   Qk  |   A   |")
         for i in range(16):
@@ -359,15 +383,20 @@ class Tomasulo:
         for i in range(32, 48):
             if self.RS[i].busy:
                 print("LOAD | {:2d} | {!r:5} | {!r:5} | {!r:6s} | {!r:5} | {!r:5} | {!r:5} | {!r:5} | {!r:5} |".format(i, self.RS[i].busy, self.RS[i].exec, self.RS[i].op, self.RS[i].Vj, self.RS[i].Vk, self.RS[i].Qj, self.RS[i].Qk, self.RS[i].A))
-        print("\nRegistradores:")
-        print("Reg | Qi | Value")
-        for i in range(11):
-            print("{:2d} | {!r:5} | {:3d} ".format(i, self.registerStat[i].Qi, self.registerStat[i].value))
-        print("\nMemoria:")
-        print("Mem | Qi | Value")
-        for i in range(5):
-            print("{:2d} | {!r:5} | {:3d} ".format(i, self.memory[i].Qi, self.memory[i].value))
         
+        print("\n16 Primeiros Registradores:")
+        print("Reg | Qi | Value")
+        for i in range(16):
+            print("{:2d} | {!r:5} | {:3d} ".format(i, self.registerStat[i].Qi, self.registerStat[i].value))
+       
+        print("\n16 Primeiras Celulas de Memoria:")
+        print("Mem | Qi | Value")
+        for i in range(16):
+            print("{:2d} | {!r:5} | {:3d} ".format(i, self.memory[i].Qi, self.memory[i].value))
+    
+    # Escreve no arquivo "output.txt" o status das Estações de Reserva ocupadas, 
+    # dos 16 Primeiros Registradores e das 16 Primeiras Celulas de Memoria
+    def writeOutputFile(self) -> None:   
         outputFile.write("Clock: {}\n".format(self.clock))
         outputFile.write("    RS    | BUSY  | Clock |   OP   |   Vj  |   Vk  |   Qj  |   Qk  |   A   |\n")
         for i in range(16):
@@ -379,57 +408,48 @@ class Tomasulo:
         for i in range(32, 48):
             if self.RS[i].busy:
                 outputFile.write("LOAD | {:2d} | {!r:5} | {!r:5} | {!r:6s} | {!r:5} | {!r:5} | {!r:5} | {!r:5} | {!r:5} |\n".format(i, self.RS[i].busy, self.RS[i].exec, self.RS[i].op, self.RS[i].Vj, self.RS[i].Vk, self.RS[i].Qj, self.RS[i].Qk, self.RS[i].A))
-        outputFile.write("\nRegistradores:\n")
+        
+        outputFile.write("\n16 Primeiros Registradores:\n")
         outputFile.write("Reg | Qi | Value\n")
-        for i in range(11):
+        for i in range(16):
             outputFile.write("{:2d} | {!r:5} | {:3d} \n".format(i, self.registerStat[i].Qi, self.registerStat[i].value))
-        outputFile.write("\nMemoria:\n")
+        
+        outputFile.write("\n16 Primeiras Celulas de Memoria:\n")
         outputFile.write("Mem | Qi | Value\n")
-        for i in range(5):
+        for i in range(16):
             outputFile.write("{:2d} | {!r:5} | {:3d} \n".format(i, self.memory[i].Qi, self.memory[i].value))
         outputFile.write("\n")
-        """
-        print("\nUnidades:")
-        print("ADD\nUnit | BUSY  | Inst")
-        for i in range(3):
-            print("{:4d} | {!r:5} | {!r:5} ".format(i, self.addUnit[i].busy, self.addUnit[i].inst))
-        print("MUL\nUnit | BUSY  | Inst")
-        for i in range(3):
-            print("{:4d} | {!r:5} | {!r:5} ".format(i, self.mulUnit[i].busy, self.mulUnit[i].inst))
-        print("LD\nUnit | BUSY  | Inst")
-        for i in range(3):
-            print("{:4d} | {!r:5} | {!r:5} ".format(i, self.ldUnit[i].busy, self.ldUnit[i].inst))
-        """
 
     # Executa o algoritmo de Tomasulo
-    def run(self):
-        #for inst in self.instList:
-        #    print(inst)
+    def run(self) -> None:
         self.search()
         self.printStatus()
         self.clock += 1
-        # input()
+        input()
         self.issue()
         while self.instrucoes > 0:
             self.write()
             self.setUnits()
             self.printStatus()
+            # self.printBusyStatus()
+            # self.writeOutputFile()
             self.search()
             self.clock += 1
-            # input()
+            input()
             self.issue()
 
+# Faz a leitura do arquivo no formato esperado
 def lexer(inputFile : TextIO) -> List[List[str]]:
     instList : List[str] = []
     
     for line in inputFile:
-        instList.append(line.replace(',', '').split()) # TODO: retirar o replace caso entrada não possua ','
+        instList.append(line.replace(',', '').split())
 
     return instList
 
 def main():
     try:
-        inputFile = open("input.txt")
+        inputFile = open("teste1.txt")
     except:
         print("Arquivo de leitura não encontrado")
         sys.exit()
